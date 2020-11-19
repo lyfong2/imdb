d8a21d750982c7bb483afca2fba1469bd32a0972
Danny Lin <dannyjabez@gmail.com>
2020-11-18 23:36:53 -0500

    fixed header logo

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
-
 const Header = () => {
     return(
         <div className="rmdb-header">
             <div className="rmdb-header-content">
-                <Link to="/">
-                    <img className="rmdb-logo" src="/public/images/reactMovie_logo.png" alt="rmdb-logo" />
-                </Link>
-                <img className="rmdb-tmdb-logo" src="/images/tmdb_logo.png" alt="tmdb-logo" />
+            <Link to={"/"} >
+                < img className={'rmdb-logo'} src={`../${process.env.PUBLIC_URL}/images/reactMovie_logo.png`} alt={'rmdb-logo'} />
+            </Link>
+            < img className={'rmdb-tmdb-logo'} src={`../${process.env.PUBLIC_URL}/images/tmdb_logo.png`} alt={"tmdb-logo"} />
             </div>
         </div>
     )
