2923040ef207273fd0c22f8f892344b4e82248a5
Danny Lin <dannyjabez@gmail.com>
2020-11-18 22:41:34 -0500

    trying to fix the logo and push on to the github

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
         <div className="rmdb-header">
             <div className="rmdb-header-content">
                 <Link to="/">
-                    <img className="rmdb-logo" src="/images/reactMovie_logo.png" alt="rmdb-logo" />
+                    <img className="rmdb-logo" src="/public/images/reactMovie_logo.png" alt="rmdb-logo" />
                 </Link>
                 <img className="rmdb-tmdb-logo" src="/images/tmdb_logo.png" alt="tmdb-logo" />
             </div>
